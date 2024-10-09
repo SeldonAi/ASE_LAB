784f8b7280eac6fdc066b444a40265cd57b656ad
SeldonAi <as.resrebus@gmail.com>
2024-10-09 07:51:25 +0200

    Commit on new secondBranch

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def add():
     a = request.args.get('a', type=float)
     b = request.args.get('b', type=float)
     if a and b:
-        return make_response(jsonify(s=a+b), 200) #HTTP 200 OK
+        return make_response(jsonify(s=a*b), 200) #HTTP 200 OK
     else:
         return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
